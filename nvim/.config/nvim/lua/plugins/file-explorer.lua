d987e0a07d205931e8bd24e4b43d873759ca774f
qiushiyan <qiushi.yann@gmail.com>
2026-09-05 22:02:38 +0100

    Set Fyler explorer width to thirty-five percent

--- nvim/.config/nvim/lua/plugins/file-explorer.lua
+++ nvim/.config/nvim/lua/plugins/file-explorer.lua
@@ -53,7 +53,7 @@ return {
       end
       return {
         kind = "split_left_most",
-        kind_presets = { split_left_most = { width = "30%" } },
+        kind_presets = { split_left_most = { width = "35%" } },
         follow_root_dir = false,
         use_as_default_explorer = true,
         integrations = { icon = "mini_icons" },
